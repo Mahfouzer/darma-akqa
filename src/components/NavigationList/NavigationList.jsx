0be64623810d56397af744913269c8d051d2041b
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-31 07:57:31 +0200

    build the navigation list and update its UI

--- src/components/NavigationList/NavigationList.jsx
+++ src/components/NavigationList/NavigationList.jsx
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavigationList() {
+  const [firstSubMenuActive, setFirstSubMenuActive] = useState(false);
   return (
     <nav className="navigation-container">
-      <ul>
+      <ul className="main-menu">
         <li>
-          <a href="/nowhere">Bedingungen</a>
+          <a
+            href="/nowhere"
+            onFocus={() => setFirstSubMenuActive(true)}
+            onMouseOver={() => setFirstSubMenuActive(true)}
+          >
+            Bedingungen
+          </a>
+          <ul className="sub-menu first-submenu-level">
+            <li>
+              <a href="/nowhere">
+                derma
+                <ul className="sub-menu second-submenu-level">
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                  <li>
+                    <a href="/nowhere">skin</a>
+                  </li>
+                </ul>
+              </a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+            <li>
+              <a href="/nowhere"> derma</a>
+            </li>
+          </ul>
         </li>
         <li>
           <a href="/nowhere">Bedingungen</a>
