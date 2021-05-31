c3c4e957dd45b2b64a6637accc6ebc11fa503d02
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-31 04:10:35 +0200

    import assets and use logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { ReactComponent as AppLogo } from "./assets/images/DermaWorld.svg";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <AppLogo />
+      </header>
     </div>
   );
 }
