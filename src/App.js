ede58a7cb392241cac78b4f47bb02698582a6b9c
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-31 04:43:40 +0200

    create app header and search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { ReactComponent as AppLogo } from "./assets/images/DermaWorld.svg";
+import AppLogo from "./assets/images/DermaWorld.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
       <header className="App-header">
-        <AppLogo />
+        <img className="App-logo" src={AppLogo} alt="Derma world" />
+        <form className="search-form">
+          <input id="search" className="search-bar" />
+          <button type="submit" className="submit-search"></button>
+        </form>
       </header>
     </div>
   );
