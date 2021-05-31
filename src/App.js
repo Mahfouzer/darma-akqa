edb4c37c3119fe061c4882c40df198da49206225
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-31 05:06:14 +0200

    create navigation component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import AppLogo from "./assets/images/DermaWorld.svg";
+import NavigationList from "./NavigationList/NavigationList";
 
 function App() {
   return (
     <div className="App-container">
       <header className="App-header">
-        <img className="App-logo" src={AppLogo} alt="Derma world" />
-        <form className="search-form">
-          <input id="search" className="search-bar" />
-          <button type="submit" className="submit-search"></button>
-        </form>
+        <div className="logo-search-container">
+          <img className="App-logo" src={AppLogo} alt="Derma world" />
+          <form className="search-form">
+            <input id="search" className="search-bar" />
+            <button type="submit" className="submit-search"></button>
+          </form>
+        </div>
+        <NavigationList />
       </header>
     </div>
   );
---
+++ src/NavigationList/NavigationList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function NavigationList() {
+  return (
+    <nav className="navigation-container">
+      <ul>
+        <li>
+          <a href="/nowhere">Bedingungen</a>
+        </li>
+        <li>
+          <a href="/nowhere">Bedingungen</a>
+        </li>
+        <li>
+          <a href="/nowhere">Bedingungen</a>
+        </li>
+        <li>
+          <a href="/nowhere">Bedingungen</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
