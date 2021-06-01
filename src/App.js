865e5f92cfccf15c5e3a550522f649178c70e370
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-06-01 13:09:22 +0200

    enhancing accessability for the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AppLogo from "./assets/images/DermaWorld.svg";
+import medicationLogo from "./assets/images/AdtralzaGlobal.svg";
 import NavigationList from "./components/NavigationList/NavigationList";
 
 function App() {
@@ -6,14 +7,47 @@ function App() {
     <div className="App-container">
       <header className="App-header">
         <div className="logo-search-container">
-          <img className="App-logo" src={AppLogo} alt="Derma world" />
-          <form className="search-form">
-            <input id="search" className="search-bar" />
-            <button type="submit" className="submit-search"></button>
+          <h1>
+            <a href="/" className="logo-container">
+              Derma world
+              <img className="App-logo" src={AppLogo} alt="Derma world" />
+            </a>
+          </h1>
+          <form className="search-form" aria-label="search the website">
+            <label>
+              <span>search the website</span>
+              <input id="search" className="search-bar" name="search" />
+            </label>
+            <button
+              type="submit"
+              className="submit-search"
+              name="submit search"
+              aria-label="submit search"
+            ></button>
           </form>
         </div>
         <NavigationList />
       </header>
+      <main>
+        <section className="medication-info">
+          <header className="medication-header">
+            <img
+              src={medicationLogo}
+              alt="Adtralza Global logo"
+              className="medication-logo"
+            ></img>
+            <h2 className="medication-about">
+              Adtralza® is a new treatment for adult patients with
+              moderate-to-severe atopic dermatitis (eczema) who are candidates
+              for systemic therapy.
+            </h2>
+            <picture>
+              <source></source>
+            </picture>
+          </header>
+          <section className="medication-about"></section>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/NavigationList/NavigationList.jsx
+++ src/components/NavigationList/NavigationList.jsx
@@ -1,49 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function NavigationList() {
-  const [firstSubMenuActive, setFirstSubMenuActive] = useState(false);
   return (
     <nav className="navigation-container">
       <ul className="main-menu">
         <li>
-          <a
-            href="/nowhere"
-            onFocus={() => setFirstSubMenuActive(true)}
-            onMouseOver={() => setFirstSubMenuActive(true)}
-          >
-            Bedingungen
-          </a>
+          <a href="/nowhere">Bedingungen</a>
           <ul className="sub-menu first-submenu-level">
             <li>
-              <a href="/nowhere">
-                derma
-                <ul className="sub-menu second-submenu-level">
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                  <li>
-                    <a href="/nowhere">skin</a>
-                  </li>
-                </ul>
-              </a>
+              <a href="/nowhere">derma</a>
+              <ul className="sub-menu second-submenu-level">
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+                <li>
+                  <a href="/nowhere">skin</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="/nowhere"> derma</a>
